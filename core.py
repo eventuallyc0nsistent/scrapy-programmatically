52680f8d4c3c115b012ef947b7e5b4d417a01524
Kiran Koduru <crazycreature11@gmail.com>
2015-01-24 12:49:45 -0500

    initial commit

---
+++ core.py
@@ -0,0 +1,26 @@
+from DmozSpider import DmozSpider
+
+# scrapy api
+from scrapy import signals, log
+from twisted.internet import reactor
+from scrapy.crawler import Crawler
+from scrapy.settings import Settings
+
+def spider_closing(spider):
+    """Activates on spider closed signal"""
+    reactor.stop()
+
+log.start(loglevel=log.DEBUG)
+settings = Settings()
+
+# crawl responsibly
+settings.set("USER_AGENT", "Kiran Koduru (+http://kirankoduru.github.io)")
+crawler = Crawler(settings)
+
+# stop reactor when spider closes
+crawler.signals.connect(spider_closing, signal=signals.spider_closed)
+
+crawler.configure()
+crawler.crawl(DmozSpider())
+crawler.start()
+reactor.run()
